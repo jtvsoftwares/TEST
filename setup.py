295fc888cb26a98f4187ce514d896d71aa1ef98f
JTV Softwares <jtvsoftwares@gmail.com>
2017-03-20 00:03:22 -0400

    Extraneous coma in setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="jtv_test",
     version="0.0",
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages(exclude=['tests'])
 )
