dcbd680219f14ae5c5406195f7900d175dcbbf8c
JTV Softwares <jtvsoftwares@gmail.com>
2017-03-19 23:02:45 -0400

    Adding setup files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="jtv_test",
+    version="0.0",
+    packages=find_packages(exclude=['tests']),
+
+    extras_require={
+        'tests': ['pytest', 'pytest-cov']
+    }
+
+)
